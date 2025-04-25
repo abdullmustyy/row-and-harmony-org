2ef7411c52b1e7577a3823c471891895abc30e4c
Abdul <abdulmusty03@outlook.com>
2025-04-25 21:12:10 +0100

    style: property page responsiveness

--- row-and-harmony/components/pages/listings/properties/property/index.tsx
+++ row-and-harmony/components/pages/listings/properties/property/index.tsx
@@ -32,19 +32,19 @@ const PropertyPage = ({ property }: IPropertyPageProps) => {
     return (
         <main className="relative isolate mt-nav">
             <section className="flex flex-col md:px-8 px-4 py-[5vh]">
-                <div className="flex items-center gap-8 pl-2">
+                <div className="flex items-center md:gap-8 gap-4 pl-2">
                     <Link href="/properties" title="Back to properties">
-                        <MoveLeft className="size-6 stroke-[2.5] stroke-primary hover:-translate-x-1 transition-transform duration-300 ease-1" />
+                        <MoveLeft className="md:size-6 size-5 stroke-[2.5] stroke-primary hover:-translate-x-1 transition-transform duration-300 ease-1" />
                     </Link>
-                    <h3 className="text-fluid-xl font-primary font-medium">{property.title}</h3>
+                    <h3 className="md:text-fluid-xl text-fluid-lg font-primary font-medium">{property.title}</h3>
                     <Button variant="ghost" className="ml-auto">
                         <Share />
-                        <span className="underline underline-offset-2">Share</span>
+                        <span className="md:inline hidden underline underline-offset-2">Share</span>
                     </Button>
                 </div>
 
-                <div className="mt-6 mb-8 grid grid-cols-[2.3fr_1fr_1fr] grid-rows-2 gap-4 items-stretch h-100">
-                    <div className="row-span-2">
+                <div className="mt-6 mb-8 grid md:grid-cols-[2.3fr_1fr_1fr] grid-cols-2 md:grid-rows-2 grid-rows-3 gap-4 items-stretch h-100">
+                    <div className="md:row-span-2 md:col-span-1 col-span-2">
                         <Image
                             src={displayImage.src}
                             alt=""
@@ -72,9 +72,9 @@ const PropertyPage = ({ property }: IPropertyPageProps) => {
                     ))}
                 </div>
 
-                <div className="flex items-start gap-20">
-                    <div className="w-2/3 grid gap-6">
-                        <div className="flex items-center justify-between">
+                <div className="flex md:flex-row flex-col items-start lg:gap-20 gap-8">
+                    <div className="md:w-2/3 w-full grid md:gap-6 gap-4">
+                        <div className="flex md:flex-row flex-col md:items-center justify-between md:gap-0 gap-4">
                             <div className="flex flex-col gap-1.5">
                                 <h4 className="font-medium text-fluid-base">
                                     {property.title} for {property.type}
@@ -84,7 +84,7 @@ const PropertyPage = ({ property }: IPropertyPageProps) => {
                                     <span>{property.location}</span>
                                 </div>
                             </div>
-                            <div className="flex flex-col gap-1.5 text-right">
+                            <div className="flex md:flex-col md:justify-normal justify-between gap-1.5 text-right">
                                 <span className="font-semibold text-fluid-base">
                                     {formatPriceNaira(property.price!)}
                                 </span>
@@ -94,12 +94,12 @@ const PropertyPage = ({ property }: IPropertyPageProps) => {
                         <p>{property.description}</p>
                     </div>
 
-                    <aside className="w-1/3 grid gap-6">
+                    <aside className="md:w-1/3 w-full grid gap-6">
                         <div>
                             <h4 className="font-semibold text-fluid-base">Get in touch</h4>
                             <p>For {property.type}s inquiries, please reach out through one of the following methods</p>
                         </div>
-                        <div className="grid grid-cols-2 gap-3">
+                        <div className="grid lg:grid-cols-2 md:grid-cols-1 grid-cols-2 gap-3">
                             <div className="text-background">
                                 <Link href="" className="link-btn justify-center w-full gap-3 *:relative">
                                     <Phone className="size-4" />
@@ -111,7 +111,7 @@ const PropertyPage = ({ property }: IPropertyPageProps) => {
                                     <span>Send a text</span>
                                 </Link>
                             </div>
-                            <div className="col-span-2 text-background">
+                            <div className="lg:col-span-2 md:col-span-1 col-span-2 text-background">
                                 <Link href="" className="link-btn justify-center w-full gap-3 *:relative">
                                     <Mail className="size-4" />
                                     <span>Send a mail</span>
