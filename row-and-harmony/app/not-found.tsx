06a5857742b92495518689bcf1274437c27d3a9b
Abdul <abdulmusty03@outlook.com>
2025-07-08 08:23:41 +0100

    chore: update images

--- row-and-harmony/app/not-found.tsx
+++ row-and-harmony/app/not-found.tsx
@@ -12,16 +12,21 @@ export const metadata: Metadata = {
 const NotFound = () => {
     return (
         <section className="h-screen grid place-content-center text-center gap-y-6">
-            <div className="flex itemsp-center gap-[1vw] text-primary text-fluid-6xl mx-auto">
+            <div className="flex items-center gap-[1vw] text-primary text-fluid-6xl mx-auto">
                 <span>4</span>
-                <Image src={Icon} alt="Icon" className="size-24 object-fill" />
+                <Image src={Icon} alt="Icon" className="h-24" />
                 <span>4</span>
             </div>
             <div className="space-y-4">
                 <h2 className="font-primary text-fluid-4xl">Are you lost?</h2>
-                <p className="text-fluid-base">Sorry, this page doesn&apos;t exist.</p>
+                <p className="text-fluid-base">
+                    Sorry, this page doesn&apos;t exist.
+                </p>
             </div>
-            <Link href="/" className="group link-btn gap-1 text-background mx-auto">
+            <Link
+                href="/"
+                className="group link-btn gap-1 text-background mx-auto"
+            >
                 <span className="relative">Back to home</span>
                 <MoveLeft className="stroke-1 group-hover:rotate-45 transition duration-600" />
             </Link>
--- row-and-harmony/components/pages/home/sections/who-we-are.tsx
+++ row-and-harmony/components/pages/home/sections/who-we-are.tsx
@@ -11,57 +11,82 @@ const WhoWeAre = () => {
         <section className="bg-background md:px-8 px-4 md:pt-[15vh] pt-[8vh] md:pb-[10vh] pb-[8vh] relative">
             <div className="flex lg:flex-row flex-col">
                 <div className="lg:w-1/2">
-                    <figure className="h-full lg:-mr-48 relative">
-                        <Image src={WhoWeAreImg} alt="Who We Are" className="object-cover size-full lg:-mt-10" />
-                        <ContainerOverlay className="opacity-50 md:block hidden" />
+                    <div className="h-full lg:-mr-48 relative">
+                        <Image
+                            src={WhoWeAreImg}
+                            alt="Who We Are"
+                            className="object-cover size-full lg:-mt-10"
+                        />
+                        <ContainerOverlay className="opacity-20 md:block hidden" />
                         <div className="absolute top-10 left-0 px-8 pt-8 pb-6 bg-accent text-background md:flex hidden flex-col gap-y-3 clip-trapezoid">
-                            <h2 className="text-fluid-2xl font-medium font-primary">10+</h2>
+                            <h2 className="text-fluid-2xl font-medium font-primary">
+                                10+
+                            </h2>
                             <span className="text-fluid-sm font-medium">
                                 Years of work
                                 <br /> experience
                             </span>
                         </div>
                         <figure className="bg-background md:block hidden lg:size-80 size-60 p-4 absolute bottom-0 lg:left-8 lg:right-auto right-8 translate-y-3/20">
-                            <Image src={CleanersAtWorkImg} alt="Cleaners at work" className="object-cover size-full" />
+                            <Image
+                                src={CleanersAtWorkImg}
+                                alt="Cleaners at work"
+                                className="object-cover size-full"
+                            />
                         </figure>
-                    </figure>
+                    </div>
                 </div>
                 <div className="bg-background lg:w-1/2 flex flex-col gap-y-6 text-fluid-sm lg:px-10 py-10 lg:z-10">
                     <SectionLabel>
                         <p>
-                            Welcome to <span className="font-medium">Row & Harmony</span>
+                            Welcome to{" "}
+                            <span className="font-medium">Row & Harmony</span>
                         </p>
                     </SectionLabel>
-                    <h2 className="text-fluid-2xl font-medium font-primary">Building Trust, Delivering Excellence</h2>
+                    <h2 className="text-fluid-2xl font-medium font-primary">
+                        Building Trust, Delivering Excellence
+                    </h2>
                     <div className="space-y-4">
                         <p>
-                            At Row & Harmony Limited, we redefine facility management with a commitment to excellence.
-                            Our solutions ensure seamless operations, from maintenance and security to sustainability
-                            and efficiency. Partner with us to enhance the functionality of your spaces.
+                            At Row & Harmony Limited, we redefine facility
+                            management with a commitment to excellence. Our
+                            solutions ensure seamless operations, from
+                            maintenance and security to sustainability and
+                            efficiency. Partner with us to enhance the
+                            functionality of your spaces.
                         </p>
                         <div className="space-y-3">
                             <p>
-                                Rooted in integrity, innovation, and service excellence, Row & Harmony is a trusted name
-                                in:
+                                Rooted in integrity, innovation, and service
+                                excellence, Row & Harmony is a trusted name in:
                             </p>
                             <ul className="list-disc marker:text-accent pl-6 space-y-1">
-                                {whoWeAreServices.map(({ name, description }, index) => (
-                                    <li key={name + index}>
-                                        <span className="font-semibold">{name}:</span> {description}
-                                    </li>
-                                ))}
+                                {whoWeAreServices.map(
+                                    ({ name, description }, index) => (
+                                        <li key={name + index}>
+                                            <span className="font-semibold">
+                                                {name}:
+                                            </span>{" "}
+                                            {description}
+                                        </li>
+                                    ),
+                                )}
                             </ul>
                             <div className="border-l-3 border-primary pl-3 py-1">
                                 <p className="text-accent">
-                                    With a strong focus on facility management, we combine modern strategies with
-                                    hands-on expertise to manage diverse properties — from corporate spaces to
-                                    residential communities.
+                                    With a strong focus on facility management,
+                                    we combine modern strategies with hands-on
+                                    expertise to manage diverse properties —
+                                    from corporate spaces to residential
+                                    communities.
                                 </p>
                             </div>
                         </div>
                     </div>
                     <Link href="/about" className="link-btn">
-                        <span className="relative text-background">Learn More About Us</span>
+                        <span className="relative text-background">
+                            Learn More About Us
+                        </span>
                     </Link>
                 </div>
             </div>
