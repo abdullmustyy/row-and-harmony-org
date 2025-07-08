f3a0a4cb47609824e12d1c6c8fa87bc134c4c032
Abdul <abdulmusty03@outlook.com>
2025-07-09 00:10:11 +0100

    style: get a quote z index

--- row-and-harmony/components/pages/home/sections/get-a-quote.tsx
+++ row-and-harmony/components/pages/home/sections/get-a-quote.tsx
@@ -5,7 +5,7 @@ type IGetAQuoteProps = React.ComponentProps<"div">;
 
 const GetAQuote = ({ className }: IGetAQuoteProps) => {
     return (
-        <section className={cn("relative z-20 bg-background isolate", className)}>
+        <section className={cn("relative bg-background isolate", className)}>
             <div className="w-9/10 translate-y-1/2 mx-auto flex md:flex-row flex-col items-center justify-between lg:gap-0 gap-8 bg-secondary lg:p-16 p-8">
                 <h2 className="text-fluid-xl font-medium font-primary md:text-left text-center">
                     Let&apos;s help you take the hassle out of
